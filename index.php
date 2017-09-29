14fcd3f33aaafb22e1242f91267adf3b6f5dc514
USP <USP@DESKTOP-TNO3FK2>
2017-09-28 21:58:07 -0500

    avanze de la clase de la semana 07

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Tarea{
+    
+    function mayordosnumeros($n1,$n2){
+        if($n1>$n2)
+                return $n1;
+        else
+            return $n2;
+}
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
